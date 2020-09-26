ef15bd4df03d23f5a47de9d263afd2653415082d
CatLemon <lqc33@qq.com>
2020-09-26 10:56:21 +0800

    替换一个不正确的I18N用法

--- src/main/java/cn/catlemon/aol_core/config/AoLCoreConfigGUI.java
+++ src/main/java/cn/catlemon/aol_core/config/AoLCoreConfigGUI.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import cn.catlemon.aol_core.AoLCore;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.fml.client.IModGuiFactory;
 import net.minecraftforge.fml.client.config.GuiConfig;
@@ -26,7 +26,7 @@ public class AoLCoreConfigGUI implements IModGuiFactory {
 	public GuiScreen createConfigGui(GuiScreen parent) {
 		return new GuiConfig(parent, ConfigElement.from(AoLCoreConfig.MainConfig.class).getChildElements(),
 				AoLCore.MODID, false, false, "Adventure of Lemon",
-				new TextComponentTranslation("gui." + AoLCore.MODID + ".config.title").getFormattedText());
+				I18n.format("gui." + AoLCore.MODID + ".config.title"));
 	}
 	
 	@Override
